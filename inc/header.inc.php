b9a238fcd9d45d218cfec301dc4cd5bb6b91f9d9
Dejan Roshkovski <d.roshkovski@gmail.com>
2018-01-26 01:51:15 +0100

    change to absolute path

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,8 +4,8 @@
 
 	<title>Placidway</title>
 
-	<link rel="stylesheet" href="../css/style.css" />
-	<link rel="stylesheet" href="../css/multi-select.css" />
+	<link rel="stylesheet" href="/css/style.css" />
+	<link rel="stylesheet" href="/css/multi-select.css" />
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
 
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include('inc/header.inc.php');
+?>
+
+
+<h2>Hello!</h2> 
+
+<p>
+Welcome to the testapp!
+</p>
+
+<p>
+If you are a student go <a href="student/index.php">here</a>.
+</p>
+
+<p>
+If you are an admin go <a href="admin/index.php">here</a>.
+</p>
+
+<?php
+include('inc/footer.inc.php');
